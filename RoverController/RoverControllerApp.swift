0162a3937996097e768ae45a27375b0dedc189fc
James Vaughan <james@jamesvaughan.ca>
2024-10-10 10:51:19 -0400

    Initial Commit

---
+++ RoverController/RoverControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoverControllerApp.swift
+//  RoverController
+//
+//  Created by James Frazier Vaughan on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct RoverControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoverControllerTests/RoverControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoverControllerTests.swift
+//  RoverControllerTests
+//
+//  Created by James Frazier Vaughan on 2024-10-10.
+//
+
+import Testing
+@testable import RoverController
+
+struct RoverControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
